e1653dad359a9ccb7776748455ca2ba2ada864f9
dushanova45 <dushanova45@gmail.com>
2021-11-15 21:26:19 +0300

    добавляю хэш-функцию

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Numerics;
+
+namespace PR2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "слово";
+            Console.WriteLine("Сообщение: {0}", text);
+            BigInteger hash = HashFunction(text);
+            Console.WriteLine("Зашифрованое сообщение: {0}", hash);
+           
+        }
+        static BigInteger HashFunction(string text)
+        {
+            char[] chars = text.ToCharArray();
+            int a = 12345363;
+            BigInteger hash = 1;
+            for (int i = 0; i < chars.Length; i++)
+            {
+                hash += (a * hash) + chars[i] * BigInteger.ModPow(10, chars.Length - 1, 1);
+            }
+            return hash;
+        }
+
+    }
+}
